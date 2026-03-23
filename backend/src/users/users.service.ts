1087474ef3fb34d75bc33626e6ac187775f4284e
Serafym Pochapynskyi <serpochapinsky@gmail.com>
2026-03-23 19:05:05 +0200

    auth-ui: add basic api methods and mocks (#5)

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
 
+interface UserData {
+  name: string;
+  email: string;
+  password: string;
+}
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+}
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: User[] = [
+    {
+      id: '1',
+      name: 'user-1',
+      email: 'serafimpoch@gmail.com',
+      password: '12345678',
+    },
+  ];
+
+  create(data: UserData) {
+    const id = crypto.randomUUID();
+    this.users.push({ id, ...data });
+
+    return { id, ...data };
+  }
+
+  findByEmail(email: string) {
+    return this.users.find((e) => e.email === email);
+  }
+}
