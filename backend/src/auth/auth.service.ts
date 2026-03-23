a4ac60e613a4fd0789279218427e38cedacbf92d
Serafim Pochapinskyi <serpochapinsky@gmail.com>
2026-03-23 19:04:34 +0200

    auth-ui: add basic api methods and mocks

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,25 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly userService: UsersService) {}
+
   register(dto: RegisterDto) {
+    const user = this.userService.findByEmail(dto.email);
+
+    if (user) {
+      throw new ConflictException();
+    }
+
+    this.userService.create(dto);
+
     return {
-      message: 'User registered successfully',
-      user: {
-        name: dto.name,
-        email: dto.email,
-      },
+      message: `User ${dto.name} with ${dto.email} was successfully created`,
     };
   }
 
   login(dto: LoginDto) {
+    const user = this.userService.findByEmail(dto.email);
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    if (dto.password !== user.password) {
+      throw new UnauthorizedException();
+    }
+
     return {
       message: 'User logged in successfully',
       accessToken: 'fake-access-token',
       user: {
-        email: dto.email,
+        email: user.email,
       },
     };
   }
